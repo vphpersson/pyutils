38749bb397b1e432442c6021d31a498b39f2dcf5
Viktor Persson <vph.persson@gmail.com>
2020-04-05 12:43:19 +0200

    Added dataclass generation and case conversion.

--- pyutils/my_dataclasses.py
+++ pyutils/my_dataclasses.py
@@ -1,8 +1,9 @@
-from dataclasses import is_dataclass
-from typing import Any, Dict, get_origin, get_args
+from dataclasses import is_dataclass, fields, make_dataclass
+from typing import Any, Dict, get_origin, get_args, Optional, List
 from abc import ABC
+from uuid import uuid4
 
-from pyutils.my_string import to_snake_case
+from pyutils.my_string import to_snake_case, to_pascal_case
 
 
 class JsonDataclass(ABC):
@@ -13,16 +14,17 @@ class JsonDataclass(ABC):
         prepared_kwargs: Dict[str, Any] = {}
 
         for key, value in kwargs.items():
-            snake_cased_key: str = to_snake_case(key)
+            snake_cased_key: str = to_snake_case(string=key)
             annotated_value_type = cls.__annotations__[snake_cased_key]
 
             if is_dataclass(annotated_value_type) and issubclass(annotated_value_type, JsonDataclass):
                 prepared_kwargs[snake_cased_key] = annotated_value_type.from_json(**value)
             elif get_origin(annotated_value_type) is list:
-                # TODO: What about a list of lists, und so weiter...?
+                
                 list_annotated_value_type = get_args(annotated_value_type)
                 if len(list_annotated_value_type) != 1:
                     raise NotImplementedError
+
                 list_annotated_value_type = list_annotated_value_type[0]
 
                 if is_dataclass(list_annotated_value_type) and issubclass(list_annotated_value_type, JsonDataclass):
@@ -36,3 +38,51 @@ class JsonDataclass(ABC):
                 prepared_kwargs[snake_cased_key] = value
 
         return cls(**prepared_kwargs)
+
+
+def dict_to_dataclass(obj, class_name: Optional[str] = None):
+
+    if isinstance(obj, dict):
+        return make_dataclass(
+            cls_name=(class_name or str(uuid4()).split('-')[-1]).replace('-', ''),
+            fields=[(to_snake_case(string=name), dict_to_dataclass(value, name)) for name, value in obj.items()]
+        )
+    elif isinstance(obj, (list, tuple)):
+        type_set = set()
+        for element in obj:
+            res = dict_to_dataclass(element)
+            type_set.add(
+                tuple((field.name, field.type) for field in fields(res)) if is_dataclass(obj=res) else res
+            )
+
+        # if not type_set:
+        return List[Any]
+        # elif len(type_set) != 1:
+        #     raise ValueError
+
+        # return next(iter(type_set))
+
+    elif isinstance(obj, (str, int, float)):
+        return type(obj)
+    elif obj is None:
+        return Any
+    else:
+        raise ValueError
+
+
+def dataclass_to_code(dataclass_class):
+    """
+
+    :param dataclass_class:
+    :return:
+    """
+
+    return '\n'.join(dataclass_to_code(field.type) for field in fields(dataclass_class) if is_dataclass(field.type)) + '\n\n' + (
+
+        f'@dataclass\n'
+        f'class {to_pascal_case(string=dataclass_class.__name__)}:\n    '
+        + ('\n    '.join(
+            f'{to_snake_case(string=field.name)}: {(to_pascal_case(string=field.type.__name__) if is_dataclass(field.type) else field.type.__name__)  if isinstance(field.type, type) else field.type}'
+            for field in fields(dataclass_class)
+        ) or 'pass')
+    )
--- pyutils/my_string.py
+++ pyutils/my_string.py
@@ -1,6 +1,28 @@
-from re import compile as re_compile
+from re import compile as re_compile, sub as re_sub
 
 _CAMEL_CASED_LETTER_PATTERN = re_compile(pattern=r'(?<=.)((?<=[a-z])[A-Z]|[A-Z](?=[a-z]))')
+
+_SNAKE_KEBAB_CASED_LETTER_PATTERN = re_compile(pattern=r'[-_]([A-Za-z])')
+
+
+def uppercase_first_character(string: str) -> str:
+    """
+
+    :param string:
+    :return:
+    """
+
+    return re_sub(pattern=r'^.', repl=lambda match: match.group(0).capitalize(), string=string)
+
+
+def lowercase_first_character(string: str) -> str:
+    """
+
+    :param string:
+    :return:
+    """
+
+    return re_sub(pattern=r'^.', repl=lambda match: match.group(0).lower(), string=string)
 
 
 def to_snake_case(string: str) -> str:
@@ -11,6 +33,32 @@ def to_snake_case(string: str) -> str:
     :return: A snake-cased representation of the provided string.
     """
 
-    return _CAMEL_CASED_LETTER_PATTERN.sub(repl=lambda match: f'_{match.group(0)}', string=string) \
+    return _CAMEL_CASED_LETTER_PATTERN.sub(repl=lambda match: f'_{match.group(0)}', string=string.replace('-', '')) \
         .replace('-', '_') \
         .lower()
+
+
+def to_pascal_case(string: str) -> str:
+    """
+    Convert a string into a Pascal-cased representation.
+
+    :param string: A camel-, snake-, or kebab-cased string.
+    :return: A Pascal-cased representation of the provided string.
+    """
+
+    return uppercase_first_character(
+        string=_SNAKE_KEBAB_CASED_LETTER_PATTERN.sub(repl=lambda match: match.group(1).upper(), string=string)
+    )
+
+
+def to_camel_case(string: str) -> str:
+    """
+    Convert a string into a camel-cased representation.
+
+    :param string: A Pascal-, kebab-, or snake-cased string.
+    :return: A camel-cased representation of the provided string.
+    """
+
+    return lowercase_first_character(
+        string=_SNAKE_KEBAB_CASED_LETTER_PATTERN.sub(repl=lambda match: match.group(1).upper(), string=string)
+    )
